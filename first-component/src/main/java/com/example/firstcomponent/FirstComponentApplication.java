4b648af5bfd6feda206002eceb76565105a1b79c
zivkovicmarko95 <zivkovic.marko.ftn@gmail.com>
2022-07-28 20:55:25 +0200

    Update maven and main

--- first-component/src/main/java/com/example/firstcomponent/FirstComponentApplication.java
+++ first-component/src/main/java/com/example/firstcomponent/FirstComponentApplication.java
@@ -1,13 +1,21 @@
 package com.example.firstcomponent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FirstComponentApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(FirstComponentApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(FirstComponentApplication.class, args);
+
+		for (int i = 0; i < 10; i++) {
+			LOGGER.info("This is small test, image testing. Iteration: {}", i);
+		}
 	}
 
 }
